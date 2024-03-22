918dedd4aaa9ad3dd1d09d139409e67fd8a32d47
PeterSaan <saanpeter@gmail.com>
2024-03-22 19:59:55 +0200

    First commit

---
+++ fetch/index.cpp
@@ -0,0 +1,13 @@
+// https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=59.44&lon=24.75
+#include <iostream>
+#include <cstdint>
+#include <memory>
+#include <string>
+
+
+using namespace std;
+
+int main()
+{
+    
+}
