a70afdc71db0c2eeece7e4fb2d27f1c31694c896
PeterSaan <saanpeter@gmail.com>
2024-04-14 17:15:23 +0300

    Ma armastan Anna-Mariat. Also did some codewars

--- practice/mdv.cpp
+++ practice/mdv.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
+void test(vector<int> arr)
+{
+    vector<int> ans = {};
+  
+  for(int i = arr[0]; i <= arr[arr.size() - 1]; i++)
+  {
+    ans.push_back(i);
+  }
+
+    for(const auto& e: ans)
+    {
+        cout << e << endl;
+    }
+}
+
 int main()
 {
-    int a = 0;
-    decltype(a) b = a;
-    b++;
-    cout << a << b;
+    test({1, 720});
 }
