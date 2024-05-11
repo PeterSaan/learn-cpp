774b362b52265a837d1e005eabb89ea28fcb3363
PeterSaan <saanpeter@gmail.com>
2024-05-11 15:28:12 +0300

    Just messing around

--- practice/mdv.cpp
+++ practice/mdv.cpp
@@ -1,25 +1,31 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <ctime>
 
 using namespace std;
 
 void test(vector<int> arr)
 {
-    vector<int> ans = {};
-  
-  for(int i = arr[0]; i <= arr[arr.size() - 1]; i++)
-  {
-    ans.push_back(i);
-  }
+	vector<int> ans = {};
 
-    for(const auto& e: ans)
-    {
-        cout << e << endl;
-    }
+	for (int i = arr[0]; i <= arr[arr.size() - 1]; i++)
+	{
+		if (i % 2 == 0)
+		{
+			ans.push_back(i);
+		}
+	}
+
+	for (const auto &e : ans)
+	{
+		cout << e << endl;
+	}
 }
 
 int main()
 {
-    test({1, 720});
+	test({1, 100});
+	// int n = rand();
+	// int* point_n = &n;
+	// cout << rand();
 }
