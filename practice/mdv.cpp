3c70d151594de24488f7cb9033e72c6f9db8a2a1
PeterSaan <saanpeter@gmail.com>
2024-07-28 02:08:52 +0300

    Leetcode problem nr. 1636

--- practice/mdv.cpp
+++ practice/mdv.cpp
@@ -1,32 +1,33 @@
+// Given an array of integers arr, sort the array in increasing oreder based on the frequency of the values.
+// If multiple values have the same frequency, sort them in decreasing order.
+// Return the sorted array.
+
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 void frequencySort(vector<int> arr) {
 	vector<int> freqArr;
-	vecotr<int> numArr;
+	vector<int> numArr;
 	vector<int> sortedArr;
 
-	for (size_t i = 0; i < arr.size(); i++) {
-		int freq = 0;
+	for (size_t i: arr) {
+		int numOccurrencesInArr = count(numArr.begin(), numArr.end(), i);
 
-		for (size_t j = 0; j < arr.size(); j++) {
-			if (arr[i] == arr[j]) {
-				freq++;
-			}
+		if (numOccurrencesInArr == 0) {
+			numArr.push_back(i);
 		}
-
-				
 	}
 
-	for (size_t i: freqOfNumArr) {
-		cout << freqOfNumArr[i] << " ";
+	for (size_t i: numArr) {
+		freqArr.push_back(count(arr.begin(), arr.end(), i));
 	}
 }
 
 int main() {
-	frequencySort({3, 3, 2, 2, 2, 2, 1});
+	frequencySort({3, 3, 2, 2, 2, 2, 1, 4});
 
 	return 0;
 }
