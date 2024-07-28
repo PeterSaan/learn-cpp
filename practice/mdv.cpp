9c4ff1ab47ae761662af5087f7551dac77a66898
PeterSaan <saanpeter@gmail.com>
2024-07-28 23:08:37 +0300

    Leetcode problem nr. 1636

--- practice/mdv.cpp
+++ practice/mdv.cpp
@@ -12,21 +12,24 @@ void frequencySort(vector<int> arr) {
 	vector<int> freqArr;
 	vector<int> numArr;
 	vector<int> sortedArr;
+	vector<vector<int>> freqOfNumArr;
 
-	for (size_t i: arr) {
-		int numOccurrencesInArr = count(numArr.begin(), numArr.end(), i);
+	for (size_t i = 0; i < arr.size(); i++) {
+		int numOccurrencesInArr = count(numArr.begin(), numArr.end(), arr[i]);
 
 		if (numOccurrencesInArr == 0) {
-			numArr.push_back(i);
+			numArr.push_back(arr[i]);
 		}
 	}
 
-	for (size_t i: numArr) {
-		freqArr.push_back(count(arr.begin(), arr.end(), i));
+	for (size_t i = 0; i < numArr.size(); i++) {
+		freqArr.push_back(count(arr.begin(), arr.end(), numArr[i]));
+		
 	}
 }
 
 int main() {
+	cout << __cplusplus;
 	frequencySort({3, 3, 2, 2, 2, 2, 1, 4});
 
 	return 0;
