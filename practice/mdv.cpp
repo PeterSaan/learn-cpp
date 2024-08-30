192a21cb5e21e02b1627f1208460e81bdbf4b7b1
PeterSaan <saanpeter@gmail.com>
2024-08-30 14:50:10 +0300

    Leetcode problem 2239

--- practice/mdv.cpp
+++ practice/mdv.cpp
@@ -1,26 +1,17 @@
-// 1460. Naje two arrays equal by reversing subarrays
-//
-// You are given two integer arrays of equal length `target` and `arr`.
-// In one step, you can select any non-empty subarray of arr and reverse it.
-// You are allowed to make any number of steps.
-// Return true if you can make arr equal to target or false otherwise.
-
 #include <iostream>
 #include <vector>
-#include <bits/stdc++.h>
 
-bool canBeEqual(std::vector<int> target, std::vector<int> arr) {
-	bool ans = false;
+int findClosestNumber(vector<int>& nums) {
+	for(auto i: nums) {
+		i = abs(i);
 
-	sort(arr.begin(), arr.end());
-	sort(target.begin(), target.end());
-	if (arr == target) {
-		ans = true;
 	}
-
-	return ans;
+	
+	std::cout << nums << std::endl;
 }
 
 int main() {
-	std::cout << canBeEqual({1, 2, 3, 4, 5, 6, 7}, {1, 7, 4, 5, 6, 3, 2}) << std::endl;
+	findClosestNumber({1, 2, 3, 4, 5});
+
+	return 0;
 }
