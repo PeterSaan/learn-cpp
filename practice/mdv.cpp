ec2fd7eb3e628145b83e8cb2d927c1b35ad6f509
PeterSaan <saanpeter@gmail.com>
2024-09-02 17:13:23 +0300

    Leetcode problem 1768

--- practice/mdv.cpp
+++ practice/mdv.cpp
@@ -1,26 +1,36 @@
+/*
+ * You are given two strings (word1 and word2).
+ * Merge the strings by adding letters in alternating order, starting with word1.
+ * If a string is longer than the other, append the additional letters onto the end of the merged string.
+ * Return the merged string.
+*/
+
 #include <iostream>
-#include <vector>
+#include <string>
 
-int findClosestNumber(std::vector<int>& nums) {
-	int ans = abs(nums[0]);
-	int index = 0;
+void mergeAlternately(std::string word1, std::string word2) {
+	std::string ans = "";
+	int diff = word1.length() - word2.length();
 
-	for (int i = 1; i < nums.size(); i++) {
-		if (abs(nums[i]) < ans) {
-			ans = abs(nums[i]);
-			index = i;
-		} else if (nums[i] == ans) {
-			index = i;
+	for (size_t i = 0; i < word1.length() - diff; i++) {
+		ans = ans + word1[i] + word2[i];
+	}
+
+	if (diff < 0) {
+		for (size_t i = word2.length() - abs(diff); i < word2.length(); i++) {
+			ans += word2[i];
+		}
+	} else if (diff > 0) {
+		for (size_t i = word1.length() - diff; i < word1.length(); i++) {
+			ans += word1[i];
 		}
 	}
 
-	return nums[index];
+	std::cout << ans << std::endl;
 }
 
 int main() {
-	std::vector<int> nums = {-4, -2, 1, 4, 8};
-
-	std::cout << findClosestNumber(nums) << std::endl;
+	mergeAlternately("ab", "pqrs");
 	
 	return 0;
 }
