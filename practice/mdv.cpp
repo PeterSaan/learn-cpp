a7879cf4c9ab8d1643ccc2d572bb5a67f4915420
PeterSaan <saanpeter@gmail.com>
2024-09-14 23:35:24 +0300

    Leetcode problem 121

--- practice/mdv.cpp
+++ practice/mdv.cpp
@@ -1,33 +1,40 @@
 /*
-* Given two strings `s` and `t`, return true if `s` is a subsequence of `t`, or false otherwise.
-* A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters
-* without disturbing the relative positions of the remaining characters. 
-* i.e., "ace" is a subsequence of "abcde" while "aec" is not.
+ * 121
+ * You're given an array of 'prices' where 'prices[i]'
+ * is the price of a given stock on the 'i'th day.
+ * Maximise your profit by choosing a single day to buy one stock
+ * and choosing a different day in the future to sell that stock.
+ * Return the maximum profit you can achieve from this transaction.
+ * If you cannot achieve any profit, return 0.
 */
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
-bool isSubsequence(std::string s, std::string t) {
-	int count = 0;
-	int correct = 0;
+int maxProfit(std::vector<int>& prices) {
+	int profit = 0;
+	int cheapestIdx = std::distance(prices.begin(), std::min_element(prices.begin(), prices.end()));
+	int cheapest = prices[cheapestIdx];
 
-	for (int i = 0; i < s.length(); i++) {
-		for (int j = count; j < t.length(); j++) {
-			if (s[i] == t[j]) {
-				correct++;
-				count = ++j;
-				break;
+	if (prices.size() != cheapestIdx + 1) {
+		int priciest = prices[cheapestIdx+1];
+		for (int i = cheapestIdx + 1; i < prices.size(); i++) {
+			if (prices[i] > priciest) {
+				priciest = prices[i];
 			}
 		}
+		profit = priciest - cheapest;
 	}
-
-	return correct == s.length();
+		
+	return profit;
 }
 
 int main() {
-	std::cout << isSubsequence("axce", "abcde") << std::endl;
+	std::vector<int> prices1 = {7, 1, 5, 3, 6, 4};
+	std::vector<int> prices2 = {7, 6, 4, 3, 1};
+	std::cout << maxProfit(prices1) << "\n";
+	std::cout << maxProfit(prices2) << std::endl;
 
 	return 0;
 }
-
-
